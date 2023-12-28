78fb6a8543d3d39a9fe4a064f9fb512adf8e370c
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2023-12-28 19:06:17 +0100

    feat: day 6

--- day6/solve.ts
+++ day6/solve.ts
@@ -5,23 +5,34 @@ import { read } from "../utils/Reader.ts"
 type Puzzle = string[][]
 
 const [task, sample] = read("day6")
-  .map((file) => file.split("\n").slice(0, -1))
-  .map((file) => file)
+  .map((file) => file.split("\n\n"))
+  .map((file) => file.map((line) => line.split("\n").filter((e) => e != "")))
 
 console.clear()
 console.log("🎄 Day 6: Custom Customs")
 
 const runPart1 = true
-const runPart2 = false
-const runBoth = false
+const runPart2 = true
+const runBoth = true
 
 /// Part 1
 
 const solve1 = (data: Puzzle) => {
-  
-  for (const row of data) {
-    console.log(row.join(""))
+  const yes = []
+
+  for (const group of data) {
+    const answers = {}
+
+    for (const answer of group) {
+      for (const question of answer.split("")) {
+        answers[question] = 1
+      }
+    }
+
+    yes.push(_.sum(_.values(answers)))
   }
+
+  return _.sum(yes)
 }
 
 const solve1Sample = runPart1 ? solve1(sample) : "skipped"
@@ -34,7 +45,28 @@ console.log("Task:\t", solve1Task)
 /// Part 2
 
 const solve2 = (data: Puzzle) => {
-  
+  const yes = []
+
+  for (const group of data) {
+    const answers = {}
+    const people = group.length
+
+    for (const answer of group) {
+      for (const question of answer.split("")) {
+        answers[question] = answers[question] || 0
+        answers[question] += 1
+      }
+    }
+
+    let allAnswered = 0
+    for (const answer of _.values(answers)) {
+      if (answer == people) allAnswered++
+    }
+
+    yes.push(allAnswered)
+  }
+
+  return _.sum(yes)
 }
 
 const solve2Sample = runPart2 ? solve2(sample) : "skipped"
