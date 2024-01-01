7c21d383b969c69358e86e8a3e6aa68a31acfe57
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-01-01 18:16:22 +0100

    feat: day 11

--- day11/solve.ts
+++ day11/solve.ts
@@ -1,6 +1,7 @@
 // @deno-types="npm:@types/lodash"
 import _, { add, flatMap, round } from "npm:lodash"
 import { read } from "../utils/Reader.ts"
+import { wait } from "../utils/utils.ts"
 
 type Puzzle = string[][]
 
@@ -12,7 +13,7 @@ console.clear()
 console.log("🎄 Day 11: Seating System")
 
 const runPart1 = true
-const runPart2 = false
+const runPart2 = true
 const runBoth = true
 
 /// Part 1
@@ -20,20 +21,43 @@ const runBoth = true
 const window = [
   [-1, -1],
   [-1, 0],
-  [-1, 1],
+  [-1, +1],
   [0, -1],
-  [0, 1],
-  [1, -1],
-  [1, 0],
-  [1, 1],
+  [0, +1],
+  [+1, -1],
+  [+1, 0],
+  [+1, +1],
 ]
 
-function occupiedAdjacents(grid: Puzzle, y: number, x: number) {
+function checkAdj(grid: Puzzle, y: number, x: number) {
   const adj = window.map(([dy, dx]) => (grid[y + dy] || [])[x + dx] || null)
   return _.countBy(adj, (t) => t)["#"] || 0
 }
 
-const solve1 = (grid: Puzzle) => {
+function checkDir(grid: Puzzle, y: number, x: number) {
+  let adj = 0
+  const [yLen, xLen] = [grid.length, grid[0].length]
+
+  for (const [dy, dx] of window) {
+    let [ny, nx] = [y, x]
+    while (_.inRange(ny, 0, yLen) && _.inRange(nx, 0, xLen)) {
+      ;[ny += dy, nx += dx]
+      if (!_.inRange(ny, 0, yLen) || !_.inRange(nx, 0, xLen)) continue
+      const tile = grid[ny][nx]
+      if (tile == ".") continue
+      if (tile == "#") adj += 1
+      break
+    }
+  }
+
+  return adj
+}
+
+const solve = (
+  grid: Puzzle,
+  checkFn: (grid: Puzzle, y: number, x: number) => number,
+  maxAdj: number,
+) => {
   const seen = new Set()
 
   while (true) {
@@ -41,9 +65,9 @@ const solve1 = (grid: Puzzle) => {
     for (const [y, row] of grid.entries()) {
       for (const [x, tile] of row.entries()) {
         if (tile == ".") continue
-        const adjOcc = occupiedAdjacents(grid, y, x)
+        const adjOcc = checkFn(grid, y, x)
         if (tile == "L" && adjOcc == 0) nGrid[y][x] = "#"
-        else if (tile == "#" && adjOcc >= 4) nGrid[y][x] = "L"
+        else if (tile == "#" && adjOcc >= maxAdj) nGrid[y][x] = "L"
       }
     }
 
@@ -59,8 +83,8 @@ const solve1 = (grid: Puzzle) => {
   }
 }
 
-const solve1Sample = runPart1 ? solve1(sample) : "skipped"
-const solve1Task = runPart1 && runBoth ? solve1(task) : "skipped"
+const solve1Sample = runPart1 ? solve(sample, checkAdj, 4) : "skipped"
+const solve1Task = runPart1 && runBoth ? solve(task, checkAdj, 4) : "skipped"
 
 console.log("\nPart 1:")
 console.log("Sample:\t", solve1Sample)
@@ -68,11 +92,8 @@ console.log("Task:\t", solve1Task)
 
 /// Part 2
 
-const solve2 = (data: Puzzle) => {
-}
-
-const solve2Sample = runPart2 ? solve2(sample) : "skipped"
-const solve2Task = runPart2 && runBoth ? solve2(task) : "skipped"
+const solve2Sample = runPart2 ? solve(sample, checkDir, 5) : "skipped"
+const solve2Task = runPart2 && runBoth ? solve(task, checkDir, 5) : "skipped"
 
 console.log("\nPart 2:")
 console.log("Sample:\t", solve2Sample)
