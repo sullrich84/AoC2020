3102431ed6134fa1cd5494203f601750b0a016ff
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-01-19 21:58:03 +0100

    chore: wip

--- day20/solve.ts
+++ day20/solve.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@types/lodash"
-import _ from "npm:lodash"
+import _, { times } from "npm:lodash"
 import { read } from "../utils/Reader.ts"
 
 type Puzzle = [number, string[]][]
@@ -20,27 +20,22 @@ console.log("🎄 Day 20: Jurassic Jigsaw")
 
 const runPart1 = true
 const runPart2 = true
-const runBoth = true
+const runBoth = false
 
 /// Part 1
 
+const top = (tile: string[]) => _.first(tile)
+const bot = (tile: string[]) => _.last(tile)
+const rgt = (tile: string[]) => tile.map((c) => _.last(c)).join("")
+const lft = (tile: string[]) => tile.map((c) => _.first(c)).join("")
+const edge = (tile: string[]) => [top(tile), rgt(tile), bot(tile), lft(tile)]
+
 const solve1 = (data: Puzzle) => {
   const tiles: Tile[] = []
-  for (const [id, grid] of data) {
-    const edges = [
-      grid[0],
-      grid[9],
-      grid.map((c) => c[0]).join(""),
-      grid.map((c) => c[9]).join(""),
-    ]
-
-    tiles.push({
-      id,
-      edges: new Set([
-        ...edges,
-        ...edges.map((e) => e.split("").reverse().join("")),
-      ]),
-    })
+  for (const [id, tile] of data) {
+    const e = edge(tile)
+    const edges = [...e, ...e.map((e) => e.split("").reverse().join(""))]
+    tiles.push({ id, edges: new Set(edges) })
   }
 
   const corners = tiles
@@ -64,53 +59,56 @@ console.log("Task:\t", solve1Task)
 
 /// Part 2
 
-const solve2 = (data: Puzzle) => {
-  const tiles = []
+const solve2 = (data: Puzzle, topLeftId: number) => {
+  const tiles: { id: string; tile: string[] }[] = []
 
   const rotate = (arr: string[]) =>
-    _.zip(...arr.reverse().map((r) => r.split(""))).map((r) => r.join(""))
+    _.zip(...arr.reverse().map((r) => r.split("")))
+      .map((r) => r.join(""))
 
-  for (const [id, g] of data) {
-    let grid = g
+  for (const [id, rawTile] of data) {
+    let tile = rawTile
     for (let rot = 0; rot < 4; rot++) {
-      const top = grid[0]
-      const bottom = grid[9]
-      const left = grid.map((r) => r[0]).join("")
-      const right = grid.map((r) => r[9]).join("")
-      tiles.push({ id, rot, flip: 0, grid, left, right, top, bottom })
-      grid = rotate(grid)
+      tiles.push({ id, tile })
+      tile = rotate(tile)
     }
 
-    let revGrid = g.map((r) => r.split("").reverse().join(""))
+    let revTile = rawTile.map((r) => r.split("").reverse().join(""))
     for (let rot = 0; rot < 4; rot++) {
-      const top = grid[0]
-      const bottom = grid[9]
-      const left = grid.map((r) => r[0]).join("")
-      const right = grid.map((r) => r[9]).join("")
-      tiles.push({ id, rot, flip: 1, grid: revGrid, left, right, top, bottom })
-      revGrid = rotate(revGrid)
+      tiles.push({ id, tile: revTile })
+      revTile = rotate(revTile)
     }
   }
 
   const len = Math.sqrt(data.length)
-  const grid = _.times(len, () => _.times(len, () => null))
+  const start = tiles.filter(({ id }) => id == topLeftId)
 
-  const rows = []
-  for (const tile of tiles) {
-    const row = [tile]
+  const photo = _.times(len, () => _.times(len, () => [""]))
+  const placed = new Set()
 
-    for (const nextTile of tiles) {
-      if (tile.id == nextTile.id) continue
-      const prev = _.last(row)!
-      if (prev.right == nextTile.left) row.push(nextTile)
+  // console.log(start)
+
+  for (let y = 0; y < len; y++) {
+    for (let x = 0; x < len; x++) {
+      // Set corner tile as starting tile
+      if (y == 0 && x == 0) {
+        photo[y][x] = start.tile
+        placed.add(start.id)
+        continue
+      }
+
+      const prevRight = (_.get(photo, [y, x - 1]) || [])
+        .map((c) => c[9]).join("")
+
+      const notPlaced = tiles.filter(({ id }) => !placed.has(id))
     }
   }
 
-  return tiles.length
+  return 0
 }
 
-const solve2Sample = runPart2 ? solve2(sample) : "skipped"
-const solve2Task = runPart2 && runBoth ? solve2(task) : "skipped"
+const solve2Sample = runPart2 ? solve2(sample, 1951) : "skipped"
+const solve2Task = runPart2 && runBoth ? solve2(task, 2801) : "skipped"
 
 console.log("\nPart 2:")
 console.log("Sample:\t", solve2Sample)
