bfd48ec11f112dcb86e192dcf820cd66e449fc27
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2024-01-20 01:25:53 +0100

    chore: wip

--- day20/solve.ts
+++ day20/solve.ts
@@ -3,7 +3,6 @@ import _, { times } from "npm:lodash"
 import { read } from "../utils/Reader.ts"
 
 type Puzzle = [number, string[]][]
-type Tile = { id: number; edges: Set<string> }
 
 const [task, sample] = read("day20")
   .map((file) => file.split("\n\n").slice(0, -1))
@@ -20,33 +19,60 @@ console.log("🎄 Day 20: Jurassic Jigsaw")
 
 const runPart1 = true
 const runPart2 = true
-const runBoth = false
+const runBoth = true
+
+type Edges = [string, string, string, string]
+type Tile = { id: number; tile: string[]; edges: Edges }
+
+function top(tile: string[]) {
+  return _.first(tile)!
+}
+
+function bottom(tile: string[]) {
+  return _.last(tile)!
+}
+
+function right(tile: string[]) {
+  return tile.map((c) => _.last(c)).join("")
+}
+
+function left(tile: string[]) {
+  return tile.map((c) => _.first(c)).join("")
+}
+
+function edges(tile: string[]) {
+  return [top(tile), right(tile), bottom(tile), left(tile)]
+}
+
+function flip(tile: string[]) {
+  return tile.map((r) => r.split("").reverse().join(""))
+}
+
+function rotate(tile: string[]) {
+  const splitted = tile.reverse().map((r) => r.split(""))
+  return _.zip(...splitted).map((r) => r.join(""))
+}
+
+function lineUp(a: string[], b: string[]) {
+  return _.intersection(a, b).length > 0
+}
+
+function adjacents(tile: Tile, tiles: Tile[]) {
+  return tiles
+    .filter((t) => t.id != tile.id)
+    .filter((t) => lineUp(t.edges, tile.edges))
+    .length
+}
 
 /// Part 1
 
-const top = (tile: string[]) => _.first(tile)
-const bot = (tile: string[]) => _.last(tile)
-const rgt = (tile: string[]) => tile.map((c) => _.last(c)).join("")
-const lft = (tile: string[]) => tile.map((c) => _.first(c)).join("")
-const edge = (tile: string[]) => [top(tile), rgt(tile), bot(tile), lft(tile)]
-
 const solve1 = (data: Puzzle) => {
-  const tiles: Tile[] = []
-  for (const [id, tile] of data) {
-    const e = edge(tile)
-    const edges = [...e, ...e.map((e) => e.split("").reverse().join(""))]
-    tiles.push({ id, edges: new Set(edges) })
-  }
+  const tiles = data.map(([id, tile]) => {
+    const allEdges = [...edges(tile), ...flip(edges(tile))]
+    return { id, tile, edges: allEdges }
+  })
 
-  const corners = tiles
-    .filter(({ id, edges: [n, e, s, w] }) =>
-      2 == tiles
-        .filter((t) => t.id != id)
-        .filter(({ edges: x }) => x.has(n) || x.has(e) || x.has(s) || x.has(w))
-        .length
-    )
-    .map(({ id }) => id)
-
+  const corners = tiles.filter((t) => adjacents(t, tiles) == 2).map((t) => t.id)
   return [corners, corners.reduce((p, c) => p * c, 1)]
 }
 
@@ -60,11 +86,7 @@ console.log("Task:\t", solve1Task)
 /// Part 2
 
 const solve2 = (data: Puzzle, topLeftId: number) => {
-  const tiles: { id: string; tile: string[] }[] = []
-
-  const rotate = (arr: string[]) =>
-    _.zip(...arr.reverse().map((r) => r.split("")))
-      .map((r) => r.join(""))
+  const tiles = []
 
   for (const [id, rawTile] of data) {
     let tile = rawTile
@@ -79,6 +101,14 @@ const solve2 = (data: Puzzle, topLeftId: number) => {
       revTile = rotate(revTile)
     }
   }
+
+  const corners = tiles
+    .filter((corner) =>
+      tiles
+        .filter(({ id }) => id != corner.id)
+        .filter(({ edges }) => lineUp(edges, corner.edges)).length == 2
+    )
+    .map(({ id }) => id)
 
   const len = Math.sqrt(data.length)
   const start = tiles.filter(({ id }) => id == topLeftId)
